85e7a53a5ef1d980e7f6b9af9135b538737aa8f7
Sai-Prakash-Reddy <161707764+Sai-Prakash-Reddy@users.noreply.github.com>
2024-10-30 15:02:16 +0530

    Added HTMl, CSS, JS files

---
+++ script.js
@@ -0,0 +1,7 @@
+function mobileMenu() {
+  const navLinks = document.getElementById('it-links');
+  navLinks.classList.toggle('show');
+
+  const mobileMenu = document.querySelector('.it-mobilemenu');
+  mobileMenu.classList.toggle('change');
+}
