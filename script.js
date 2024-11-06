f9599a4aa9bd67584f9cb0f2db93da4ac2a50d3d
Sai-Prakash-Reddy <161707764+Sai-Prakash-Reddy@users.noreply.github.com>
2024-11-07 01:04:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function mobileMenu() {
   const mobileMenu = document.querySelector('.it-mobilemenu');
   mobileMenu.classList.toggle('change');
 }
+
+function copyText() {
+  let text = document.getElementById("p1").textContent;
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert('Text copied to clipboard!');
+    })
+    .catch(err => {
+      console.error('Error copying text: ', err);
+    });
+}
