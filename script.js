f8cad46eccdfcf17f98abba69b3dc8e393c6ac27
Sai-Prakash-Reddy <161707764+Sai-Prakash-Reddy@users.noreply.github.com>
2024-11-07 01:13:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function copyText() {
       console.error('Error copying text: ', err);
     });
 }
+
+function copyText(id) {
+  let text = document.getElementById(id).textContent;
+  
+  // Check if clipboard API is supported
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text) 
+      .then(() => {
+        alert('Text copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Error copying text: ', err); 
+        alert("An error occurred while copying text.");
+      });
+  } else {
+    console.error("Clipboard API is not supported on this browser.");
+    alert("Clipboard functionality is not supported in this environment.");
+  }
+}
